020870323d96b57fce671cb876b40fc2cac91684
victorrobles97 <vrobles@pucp.pe>
2020-04-17 06:31:05 +0000

    15 de abril

--- Githubpc.R
+++ Githubpc.R
@@ -1,4 +1,4 @@
-###Scrapping
+###Scrapping ->Manera de poder jaalr contenido de internet para el R
 
 linkPage= "https://en.wikipedia.org/wiki/2019%E2%80%9320_coronavirus_pandemic"
 linkPath = '//*[@id="thetable"]'
